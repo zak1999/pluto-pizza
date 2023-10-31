7f2fbd41924841bba5f5f9abfd70eea2e1b41a20
zak1999 <zakariyya_ahmed@outlook.com>
2023-10-31 15:57:18 +0000

    refactor: nextConfig rerout

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/pizza',
+        permanent: true,
+      },
+    ]
+  },
 }
-
 module.exports = nextConfig
